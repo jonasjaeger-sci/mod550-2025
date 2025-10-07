c8798432cdbd6c1156b5bf2ff83b129388ecf016
jonas <jonas.jaeger1@gmx.de>
2025-10-07 12:55:27 +0200

    Commit 1

--- Exercises/IrisLogisticRegression.py
+++ Exercises/IrisLogisticRegression.py
@@ -4,23 +4,48 @@ from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 
+# initiate random seed
+r_seed = 77
 
-# first solve with decision tree
+# acquire and prepare data
 iris = load_iris()
 X = iris.data
-y = iris.target
+Y = iris.target
 #print(X.shape)
-#print(y.shape)
-plt.figure(figsize=(10,5), dpi= 60)
-clf = DecisionTreeClassifier(max_leaf_nodes=10, criterion="entropy")
+#print(Y.shape)
+x_train, x_test, y_train, y_test = train_test_split(
+    X, Y, test_size=0.3, random_state=r_seed)
 
-clf.fit(X, y)
-plot_tree(clf, proportion=True, filled=True)
+# first solve with decision tree
+DT_clf = DecisionTreeClassifier(max_leaf_nodes=10,
+                                criterion="entropy",
+                                random_state=r_seed)
+DT_clf.fit(x_train, y_train)
+plt.figure(figsize=(10,5), dpi= 60)
+plot_tree(DT_clf, proportion=True, filled=True)
+y_pred_DT = DT_clf.predict(x_test)
+acc_DT = accuracy_score(y_test, y_pred_DT)
+print(f"Accuracy of Decision Tree: {acc_DT:.3f}")
+
+# solve with random forest
+RF_clf = RandomForestClassifier(n_estimators=100,
+                                random_state=r_seed,
+                                n_jobs=-1)
+RF_clf.fit(x_train,y_train)
+y_pred_RF = RF_clf.predict(x_test)
+acc_RF = accuracy_score(y_test, y_pred_RF)
+print(f"Accuracy of Random Forest: {acc_RF:.3f}")
 
 # solve with logistic regression
-Lr = LogisticRegression()
-Lr.fit(X,y)
-Lr.score(X,y)
+Lr_clf = LogisticRegression(random_state=r_seed,
+                            max_iter=10,
+                            solver="newton-cholesky")
+Lr_clf.fit(x_train, y_train)
+y_pred_LR = Lr_clf.predict(x_test)
+acc_LR = accuracy_score(y_test, y_pred_LR)
+print(f"Accuracy of Logistic Regression: {acc_DT:.3f}")
 
-plt.show()
+#plt.show()
